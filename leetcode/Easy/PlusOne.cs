7cbdd3d460a38b5cc231b55c43999c7696a4db5b
Sai Krishna Podduturi <skrx7392@gmail.com>
2023-09-23 19:36:14 -0700

    Added Leetcode 66 - Plus One

---
+++ leetcode/Easy/PlusOne.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Easy
+{
+  /// <summary>
+  /// Leetcode 66 - https://leetcode.com/problems/plus-one
+  /// </summary>
+  public class PlusOne
+  {
+    public int[] run(int[] digits)
+    {
+      if (digits[digits.Length - 1] != 9)
+      {
+        digits[digits.Length - 1] = digits[digits.Length - 1] + 1;
+        return digits;
+      }
+      int carryover = digits[digits.Length - 1] == 9 ? 1 : 0;
+      int[] result = new int[digits.Length + 1];
+      int index = digits.Length - 1;
+      while(index >= 0)
+      {
+        result[index + 1] = (digits[index] + carryover) % 10;
+        if (digits[index] + carryover > 9) {
+          carryover = 1;
+        }
+        else
+        {
+          carryover = 0;
+        }
+        index--;
+      }
+      if(carryover == 0)
+      {
+        return result.Skip(1).ToArray();
+      }
+      result[0] = carryover;
+      return result;
+    }
+  }
+}
---
+++ leetcodeTests/Easy/PlusOneTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using leetcode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Easy.Tests
+{
+  [TestFixture()]
+  public class PlusOneTests
+  {
+    [Test()]
+    public void runTest1()
+    {
+      var plusOne = new PlusOne();
+      int[] input = new int[] { 1, 2, 3 };
+      var output = plusOne.run(input);
+      var expected = new int[] { 1, 2, 4 };
+      Assert.AreEqual(expected, output);
+    }
+
+    [Test()]
+    public void runTest2()
+    {
+      var plusOne = new PlusOne();
+      int[] input = new int[] { 4, 3, 2, 1 };
+      var output = plusOne.run(input);
+      var expected = new int[] { 4, 3, 2, 2 };
+      Assert.AreEqual(expected, output);
+    }
+
+    [Test()]
+    public void runTest3()
+    {
+      var plusOne = new PlusOne();
+      int[] input = new int[] { 9 };
+      var output = plusOne.run(input);
+      var expected = new int[] { 1, 0 };
+      Assert.AreEqual(expected, output);
+    }
+
+    [Test()]
+    public void runTest4()
+    {
+      var plusOne = new PlusOne();
+      int[] input = new int[] { 7, 9 };
+      var output = plusOne.run(input);
+      var expected = new int[] { 8, 0 };
+      Assert.AreEqual(expected, output);
+    }
+
+    [Test()]
+    public void runTest5()
+    {
+      var plusOne = new PlusOne();
+      int[] input = new int[] { 9, 8, 9 };
+      var output = plusOne.run(input);
+      var expected = new int[] { 9, 9, 0 };
+      Assert.AreEqual(expected, output);
+    }
+  }
+}
